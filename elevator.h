0d8bf860e83c8e2942052aea6a9f60dbc14053a8
subliminalj <jshtz8@mail.umkc.edu>
2016-04-01 20:40:34 -0500

    added setters to elevator.h

--- elevator.h
+++ elevator.h
@@ -17,14 +17,26 @@ private:
 	std::list<rider> waiting_list;
 
 public:
-	elevator();
-	elevator();
+	elevator(int floors, double arrival) {
+		floornum = floors;
+		arrival_rate = arrival;
+	}
+		
+		
 	int get_floornum() { return floornum; }
 	int get_total_served() { return total_served; }
 	double get_arrival_rate() { return arrival_rate; }
 	std::list<rider> get_riders() { return rider_list; }
 	std::list<rider> get_waiting() { return waiting_list; }
+	void set_floornum(int floor) { floornum = floor; }
+	void add_total_served() { total_served++; }
+	void set_arrival_rate(double arrive) { arrival_rate = arrive; }
+	void set_riders(std::list<rider> riders) { rider_list = riders; }
+	void set_waiting(std::list<rider> waiting) { waiting_list = waiting; }
 	bool empty() { return rider_list.empty(); }
+	update() {}
+
+
 };
 
 
